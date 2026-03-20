e8d326459e8dfcefaf7d89bc1d24f77a3539f177
LouisEtienneLemire <louis.lemonarch@gmail.com>
2026-03-20 16:25:53 -0400

    tri backend

--- obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+017f81533a349f96a37850e09bd3b70bed6aa7f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e467e44f6924758da7108a8b6c463d35e3d6bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
